f013551c859898e1b44875b6eb914feb8a6a3522
Stefan0712 <s.vladulescu@gmail.com>
2025-01-27 13:36:18 +0200

    Added an image of the wordpress blog

--- src/projects/WordpressBlog.js
+++ src/projects/WordpressBlog.js
@@ -1,7 +1,11 @@
+import blogImg from './blog-content.png';
+
 const WordpressBlog = () => {
     return ( 
         <div className="project fitness-app-project">
             <h1>Work in progress</h1>
+            <h2>Homepage</h2>
+            <img src={blogImg} alt="" style={{width: '50%', height: 'auto'}}/>
         </div>
      );
 }
